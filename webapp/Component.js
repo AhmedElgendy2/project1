7392964b86427ef3acd70226fe3aa31a5f0b69a3
Ahmed Elgendy <ahmedelgendy472@gmail.com>
2026-04-02 14:15:11 +0000

    First Commi

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.intern.northwind.productapp.project1.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
